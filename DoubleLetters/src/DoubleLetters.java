7f1a62314c2f823980943991a3acf9aa768b52e2
Gabriel Jones <gjones2023@chadwickschool.org>
2021-09-15 11:34:11 -0700

    feat: Create a double letter program
    This program finishes the specified project

--- DoubleLetters/src/DoubleLetters.java
+++ DoubleLetters/src/DoubleLetters.java
@@ -1,40 +1,42 @@
 import java.util.Scanner;
+
 public class DoubleLetters {
 	public static void main(String[] args) {
-	Scanner scan = new Scanner(System.in);
-	String string = scan.nextLine();
-	int pointer = 0;
-	String substring1 = string.substring(pointer,pointer+1);
-	String changedString = "";
-	
-	for(int i = 0;i < string.length(); i++) 
-	if(string.substring(pointer,pointer+1).equals("!") == true) {
-			changedString = substring1 + substring1 + substring1;
-			
-			if(pointer <= string.length()-2) {
-			substring1 = string.substring(pointer,pointer+1);
+		Scanner scan = new Scanner(System.in);
+		String startingString = scan.nextLine();
+		String doubled = "";
+		
+		for(int i = 0 ; i< startingString.length(); i++) {
+			char c = startingString.charAt(i);
+			if(startingString.substring(i,i+1).equals("!")) {
+				doubled = "!!!";
+				
+			}
+			else if(Character.isLetter(c)) {
+				if(i<startingString.length()) {
+				doubled = startingString.substring(i,i+1) + startingString.substring(i,i+1);
+				
+				}
+				else {
+					doubled = startingString.substring(i) + startingString.substring(i);
+				}
+				
+				
 			}
 			else {
-				substring1 = string.substring(pointer);
+				if(i<startingString.length()) {
+					doubled = startingString.substring(i,i+1);
+					
+					}
+					else {
+						doubled = startingString.substring(i);
+					}
+				
 			}
-			System.out.print(changedString);
-			pointer++;
+			System.out.print(doubled);
 		}
-	else if(pointer <= string.length()) {
-		changedString = substring1 + substring1;
 		
-		if(pointer <= string.length()){
-		substring1 = string.substring(pointer,pointer+1);
-		}
-		else {
-			substring1 = string.substring(pointer);
-		}
-		System.out.print(changedString);
 		
-		pointer++;
 	}
-	
-	}
-	
-	
 }
+	
